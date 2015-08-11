7020f9ff21ee6a5c8080c0bf8a33b95458d60dab
Kai Pak <kai@puppetlabs173.local>
2015-08-11 11:44:50 -0700

    Create tarball shell script and actual tarball.  Minor edits to init.pp

--- tse_toolkit/manifests/init.pp
+++ tse_toolkit/manifests/init.pp
@@ -4,7 +4,7 @@ class tse_toolkit (
   $vagrant_version  = $tse_toolkit::params::vagrant_version,
   $vagrant_plugins  = $tse_toolkit::params::vagrant_plugins,
   $user             = $tse_toolkit::params::user,
-  $pe_demo_url          = $tse_toolkit::params::pe_demo_url, 
+  $pe_demo_url      = $tse_toolkit::params::pe_demo_url, 
 ) inherits tse_toolkit::params {
 
   class { tse_toolkit::env: 
