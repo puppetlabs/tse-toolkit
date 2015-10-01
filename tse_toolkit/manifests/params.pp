b226b2e3925b7a354fff768a6948105ce7b41652
Kai Pak <kai@kaipak.org>
2015-10-01 15:27:21 -0700

    Updated VirtualBox Version

--- tse_toolkit/manifests/params.pp
+++ tse_toolkit/manifests/params.pp
@@ -4,8 +4,8 @@ class tse_toolkit::params {
   $pe_version     = '2015.2.x'
   $pe_demo_url    = "http://tse-builds.s3-us-west-2.amazonaws.com/${pe_version}/commits/pe-demo-latest.tar.gz"
   
-  $vbox_version     = '5.0.0'
-  $vbox_patch_level = '101573'
+  $vbox_version     = '5.0.4'
+  $vbox_patch_level = '102546'
 
   $vagrant_version = '1.7.4' 
   $vagrant_plugins = [ 'oscar', 'vagrant-hosts', 'vagrant-multiprovider-snap', 'vagrant-reload']
