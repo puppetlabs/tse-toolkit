d5901e4dcb16000a45f89c153d9d4f612c39f50d
Kai Pak <kai@kaipak.org>
2016-04-13 11:18:15 -0700

    update to 2016.1 and update vbox and vagrant version

--- tse_toolkit/manifests/params.pp
+++ tse_toolkit/manifests/params.pp
@@ -1,13 +1,13 @@
 class tse_toolkit::params {
 
   $user           = 'username'
-  $pe_version     = '2015.3.x'
+  $pe_version     = '2016.1.x'
   $pe_demo_url    = "http://tse-builds.s3-us-west-2.amazonaws.com/${pe_version}/releases/pe-demo-latest.tar.gz"
 
-  $vbox_version     = '5.0.12'
-  $vbox_patch_level = '104815'
+  $vbox_version     = '5.0.16'
+  $vbox_patch_level = '105871'
 
-  $vagrant_version = '1.7.4' 
+  $vagrant_version = '1.8.1' 
   $vagrant_plugins = [ 'oscar', 'vagrant-hosts', 'vagrant-multiprovider-snap', 'vagrant-reload', 'vagrant-cachier']
 
   $packages = [ "Vagrant_${vagrant_version}",
