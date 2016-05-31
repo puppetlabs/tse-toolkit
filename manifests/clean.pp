ecbeb4ec8febc0d67eb68283bb83eb37d89b6920
Kai Pak <kaipak@users.noreply.github.com>
2016-05-31 13:49:23 -0700

    Update clean.pp

--- manifests/clean.pp
+++ manifests/clean.pp
@@ -4,14 +4,14 @@ class toolkit::clean {
   exec { 'clean-vagrant-install':
     command => "/bin/rm /var/db/.puppet_pkgdmg_installed_Vagrant*",
     onlyif  => [
-                "/bin/test -f /var/db/.puppet_pkgdmg_installed_Vagrant*",
+                "/bin/test -f /var/db/.puppet_pkgdmg_installed_Vagrant.*",
                 "/bin/test ! -f /usr/local/bin/vagrant"
                ],
   } 
   exec { 'clean-vbox-install':
     command => "/bin/rm /private/var/db/.puppet_pkgdmg_installed_VirtualBox*",
     onlyif  => [
-                "/bin/test -f /private/var/db/.puppet_pkgdmg_installed_VirtualBox*",
+                "/bin/test -f /private/var/db/.puppet_pkgdmg_installed_VirtualBox.*",
                 "/bin/test ! -d /Applications/VirtualBox.app/"
                ],
   }
