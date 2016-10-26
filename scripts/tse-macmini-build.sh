c8a3823da215926b3f064e9ee8a8b21e6f4975d7
Kai Pak <kai@kaipak.org>
2016-10-26 12:15:35 -0400

    Run bootstrap script as user rather than root

--- scripts/tse-macmini-build.sh
+++ scripts/tse-macmini-build.sh
@@ -4,5 +4,5 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/bin
 cd ~
 curl -sL http://git.io/tse-toolkit | sudo ruby 
 cd ~/vagrant_tse_demos/pe-demo*
-~/vagrant_tse_demos/pe-demo*/scripts/init.sh >> ~/vagrant_tse_demos/init.log
+sudo -u tseadmin ~/vagrant_tse_demos/pe-demo*/scripts/init.sh >> ~/vagrant_tse_demos/init.log
 vagrant puppetize | sudo /opt/puppetlabs/puppet/bin/puppet apply
