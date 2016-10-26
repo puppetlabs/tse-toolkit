ad7e5343ac6e5e2a133e3cc15b06f9ac80bbdfdf
Kai Pak <kai@kaipak.org>
2016-10-26 12:43:09 -0400

    wtf

--- scripts/tse-macmini-build.sh
+++ scripts/tse-macmini-build.sh
@@ -4,6 +4,7 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/bin
 cd /Users/tseadmin
 curl -sL http://git.io/tse-toolkit | sudo ruby 
 cd /Users/tseadmin/vagrant_tse_demos/pe-demo*
+sleep 2
 chown tseadmin .vagrant
 sudo -S -u tseadmin -i /bin/bash -l -c '/Users/tseadmin/vagrant_tse_demos/pe-demo*/scripts/init.sh >> /Users/tseadmin/vagrant_tse_demos/init.log'
 vagrant puppetize | sudo /opt/puppetlabs/puppet/bin/puppet apply
