07bb0fb439cf4d59fd0fb89df2415307ca4b066a
Kai Pak <kaipak@users.noreply.github.com>
2016-10-28 11:26:48 -0400

    Missing keyword in puppetize line.

--- scripts/tse-macmini-build.sh
+++ scripts/tse-macmini-build.sh
@@ -7,4 +7,4 @@ cd /Users/$USER
 curl -sL http://git.io/tse-toolkit | sudo ruby 
 cd /Users/$USER/vagrant_tse_demos/pe-demo*
 sudo -u $USER /Users/$USER/vagrant_tse_demos/pe-demo*/scripts/init.sh >> /Users/$USER/vagrant_tse_demos/init.log
-sudo -u $USER vagrant puppetize | sudo /opt/puppetlabs/puppet/bin/puppet apply
+sudo -u $USER vagrant hosts puppetize | sudo /opt/puppetlabs/puppet/bin/puppet apply >> /Users/$USER/vagrant_tse_demos/init.log
