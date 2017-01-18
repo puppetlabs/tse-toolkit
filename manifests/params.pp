7a7373c3dc9172cb736e4b547e83755837681b8d
maju6406 <beersy@gmail.com>
2017-01-18 14:24:46 -0800

    Updating versions

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 class toolkit::params {
   $user             = 'username'
-  $pe_version       = '2016.1.x'
+  $pe_version       = '2016.5.x'
   $pe_demo_url      = "http://tse-builds.s3-us-west-2.amazonaws.com/${pe_version}/releases/pe-demo-latest.tar.gz"
-  $vbox_version     = '5.0.16'
-  $vbox_patch_level = '105871'
-  $vagrant_version  = '1.8.1' 
+  $vbox_version     = '5.1.14'
+  $vbox_patch_level = '112924'
+  $vagrant_version  = '1.9.1'
   $vagrant_plugins  = [ 'oscar', 'vagrant-hosts', 'vagrant-multiprovider-snap', 'vagrant-reload', 'vagrant-cachier']
   $packages         = [ "Vagrant_${vagrant_version}", "VirtualBox-${vbox_version}-${vbox_patch_level}"]
 }
