514afd5284b9d3b068b20cd393aa933c07be976e
maju6406 <beersy@gmail.com>
2017-01-19 10:29:40 -0800

    Reverting to vagrant 1.9.0 due to some networking issues

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@ class toolkit::params {
   $pe_demo_url      = "http://tse-builds.s3-us-west-2.amazonaws.com/${pe_version}/releases/pe-demo-latest.tar.gz"
   $vbox_version     = '5.1.14'
   $vbox_patch_level = '112924'
-  $vagrant_version  = '1.9.1'
+  $vagrant_version  = '1.9.0'
   $vagrant_plugins  = [ 'oscar', 'vagrant-hosts', 'vagrant-multiprovider-snap', 'vagrant-reload', 'vagrant-cachier']
   $packages         = [ "Vagrant_${vagrant_version}", "VirtualBox-${vbox_version}-${vbox_patch_level}"]
 }
