b067e2138a781e4efd1f12d3cf03ecd54f1a9dc4
Chris Barker <cbarker@puppetlabs.com>
2018-09-19 18:43:13 -0700

    killing the build script that is being run
    someone is calling launchd somewhere that is calling this script

--- scripts/tse-macmini-build.sh
+++ scripts/tse-macmini-build.sh
@@ -3,8 +3,8 @@
 USER=tseadmin
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/bin
 
-cd /Users/$USER
-curl -sL http://git.io/tse-toolkit | sudo ruby 
-cd /Users/$USER/vagrant_tse_demos/pe-demo*
-sudo -u $USER /Users/$USER/vagrant_tse_demos/pe-demo*/scripts/init.sh >> /Users/$USER/vagrant_tse_demos/init.log
-sudo -u $USER vagrant hosts puppetize | sudo /opt/puppetlabs/puppet/bin/puppet apply >> /Users/$USER/vagrant_tse_demos/init.log
+#cd /Users/$USER
+#curl -sL http://git.io/tse-toolkit | sudo ruby 
+#cd /Users/$USER/vagrant_tse_demos/pe-demo*
+#sudo -u $USER /Users/$USER/vagrant_tse_demos/pe-demo*/scripts/init.sh >> /Users/$USER/vagrant_tse_demos/init.log
+#sudo -u $USER vagrant hosts puppetize | sudo /opt/puppetlabs/puppet/bin/puppet apply >> /Users/$USER/vagrant_tse_demos/init.log
